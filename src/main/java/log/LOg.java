9ad2c5879c5eb4d702e4e99a459a49df599fc8c5
Denis Volkov <volkov.denis.alekseevish@mail.ru>
2020-02-18 20:36:31 +0500

    цу

---
+++ src/main/java/log/LOg.java
@@ -0,0 +1,37 @@
+package log;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LOg {
+	
+	private LOg() {}
+	
+	public static void logToFile(Throwable e) {
+		  File logFolder = new File("log");
+		  if (!logFolder.exists()) logFolder.mkdir();
+		  
+		Logger logger = Logger.getLogger("MyLog");
+		   FileHandler fh = null;
+		        SimpleDateFormat format = new SimpleDateFormat("dd-MM-YYYY HH_mm_ss");
+		        try {
+		            fh = new FileHandler("log/"+format.format(Calendar.getInstance().getTime()) + ".log");
+		        } catch (Exception e2) {
+		            e2.printStackTrace();
+		        }
+		        fh.setFormatter(new SimpleFormatter());
+		        logger.addHandler(fh);
+		        
+		        StringWriter sw = new StringWriter();
+		        PrintWriter pw = new PrintWriter(sw);
+		        	e.printStackTrace(pw);
+		        		logger.severe(sw.toString());
+		        		fh.close();
+	}
+}
